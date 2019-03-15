7df18e8bd9e4e237baf074390f6518506b7e993a
Machine100 <joesmileyyy@yahoo.com>
2019-03-15 10:02:47 -0400

    Creating basic react structure.

---
+++ src/components/GoFetch.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class GoFetch extends Component {
+	state = {}
+	render(){
+		return <h1>Hihope</h1>;
+	}
+}
+
+export default GoFetch;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import GoFetch from './components/GoFetch';
+
+ const element = <h1>Hi</h1>;
+ ReactDOM.render(<GoFetch />, document.getElementById('root'));
