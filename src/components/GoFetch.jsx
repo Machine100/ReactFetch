a9f3f0d6f49f5290722535e678579719c7acefe6
Machine100 <joesmileyyy@yahoo.com>
2019-03-15 13:27:14 -0400

    Adding call to back end

--- src/components/GoFetch.jsx
+++ src/components/GoFetch.jsx
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
 
 class GoFetch extends Component {
-	state = {}
+	state = {
+		count: 0,
+		monsterpic: 'https://sowfpbj0a4.execute-api.us-east-1.amazonaws.com/stagename1/pets/'
+	}
 	render(){
-		return <h1>Hihope</h1>;
+		const {count} = this.state
+		const {monsterpic} = this.state
+		return(
+			<React.Fragment>
+				//<img src={this.state.monsterpic} alt='' />
+				//<span>{monsterpic}</span>
+				<span>{count}</span>
+				<button>innertext</button>
+			</React.Fragment>
+		);
 	}
 }
 
 export default GoFetch;
+
+
+//inside the render is a return
