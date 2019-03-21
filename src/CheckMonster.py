97d6b58a4c420a288aa5b1f2beb4a4bf294b3959
Machine100 <joesmileyyy@yahoo.com>
2019-03-21 15:47:40 -0400

    Create CheckMonster.py

---
+++ src/CheckMonster.py
@@ -0,0 +1,21 @@
+
+# This code is intended to be deployed at AWS lambda
+
+import boto3
+s3client = boto3.client('s3')
+
+
+
+
+
+#if fedscribble exists
+	#swap to unfed
+
+  import boto3
+    s3client = boto3.client('s3')
+
+    response = s3client.list_objects(Bucket='asdfasdf-s3bucket-tftui51y564p')
+    print('response::::', response)
+
+
+https://s3.amazonaws.com/derffred/scribble.jpg
