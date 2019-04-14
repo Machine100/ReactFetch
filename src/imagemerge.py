a5e2abb18a995c440e48f0ebfb31b6e7f0bbe05a
machine100 <joesmileyyy@yahoo.com>
2019-04-14 13:39:56 -0700

    Update imagemerge.py

--- src/imagemerge.py
+++ src/imagemerge.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
-img = image.new("RGB",(640,480),(0,0,255))
+img = Image.new("RGB",(640,480),(0,0,255))
 
 
-#background = image.open("test1.png")
-#foreground = image.open("test2.png")
+#background = Image.open("test1.png")
+#foreground = Image.open("test2.png")
 
 #background.paste(foreground, (0, 0), foreground)
 #background.show()
