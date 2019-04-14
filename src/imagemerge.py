5fa5cdd302acb2623ae7c50168e38fbc33b8cb9c
machine100 <joesmileyyy@yahoo.com>
2019-04-14 15:19:22 -0700

    fleshing out step function

--- src/imagemerge.py
+++ src/imagemerge.py
@@ -3,11 +3,10 @@ from PIL import Image
 #img = Image.new("RGB",(640,480),(0,0,255))
 
 
-background = Image.open("cat1.png")
-foreground = Image.open("cat2.png")
+background = Image.open("house1.png")
+foreground = Image.open("house2.png")
 background.paste(foreground, (0, 0), foreground)
 
-foreground = Image.open("cat3.png")
-background.paste(foreground, (0, 0), foreground)
+
 
 background.show()	
