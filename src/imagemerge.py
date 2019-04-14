f3909407efc77cd5472da21934ed6b412818343a
machine100 <joesmileyyy@yahoo.com>
2019-04-14 14:19:32 -0700

    adding in image merge

--- src/imagemerge.py
+++ src/imagemerge.py
@@ -1,10 +1,13 @@
 from PIL import Image
 
-img = Image.new("RGB",(640,480),(0,0,255))
+#img = Image.new("RGB",(640,480),(0,0,255))
 
 
-#background = Image.open("test1.png")
-#foreground = Image.open("test2.png")
+background = Image.open("cat1.png")
+foreground = Image.open("cat2.png")
+background.paste(foreground, (0, 0), foreground)
 
-#background.paste(foreground, (0, 0), foreground)
-#background.show()
+foreground = Image.open("cat3.png")
+background.paste(foreground, (0, 0), foreground)
+
+background.show()	
