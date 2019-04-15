461e12262edbaafbb0533163f6530485ba7a98f4
Machine100 <joesmileyyy@yahoo.com>
2019-04-15 09:31:00 -0400

    Update CheckMonster.py

--- src/CheckMonster.py
+++ src/CheckMonster.py
@@ -1,21 +1,17 @@
 
-# This code is intended to be deployed at AWS lambda
-
 import boto3
 s3client = boto3.client('s3')
 
 
+if state[things cat needs]:
+	state[cat1: true]
+if state[things cat needs]:
+	state[cat2: true]
+if state[things cat needs]:
+	state[cat3: true]
+if state[things cat needs]:
+	state[cat4: true]
+if state[things cat needs]:
+	state[cat5: true]
+	
 
-
-
-#if fedscribble exists
-	#swap to unfed
-
-  import boto3
-    s3client = boto3.client('s3')
-
-    response = s3client.list_objects(Bucket='asdfasdf-s3bucket-tftui51y564p')
-    print('response::::', response)
-
-
-https://s3.amazonaws.com/derffred/scribble.jpg
