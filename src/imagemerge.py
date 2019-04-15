654271285c28b70a2b8916521fada4082e50dd7f
Machine100 <joesmileyyy@yahoo.com>
2019-04-15 09:22:08 -0400

    Update imagemerge.py

--- src/imagemerge.py
+++ src/imagemerge.py
@@ -15,28 +15,39 @@ cat5  = Image.open("")
 
 
 if state[]:
+	state[food1: true]
 	mainimage.paste(food1, (0, 0), food1)
 if state[]:
+	state[food2: true]
 	mainimage.paste(food2, (0, 0), food2)
 if state[]:
+	state[toy1: true]
 	mainimage.paste(toy1,  (0, 0), toy1)
 if state[]:
+	state[toy2: true]
 	mainimage.paste(toy2,  (0, 0), toy2)
 if state[]:
+	state[toy3: true]
 	mainimage.paste(toy3,  (0, 0), toy3)
 if state[]:
+	state[cat1: true]
 	mainimage.paste(cat1,  (0, 0), cat1)
 if state[]:
+	state[cat2: true]
 	mainimage.paste(cat2,  (0, 0), cat2)
 if state[]:
+	state[cat3: true]
 	mainimage.paste(cat3,  (0, 0), cat3)
 if state[]:
+	state[cat4: true]
 	mainimage.paste(cat4,  (0, 0), cat4)
 if state[]:
+	state[cat5: true]
 	mainimage.paste(cat5,  (0, 0), cat5)
 
-background = Image.open("house1.png")
-foreground = Image.open("house2.png")
-background.paste(foreground, (0, 0), foreground)
+mainimage.show()
 
-background.show()	
+#background = Image.open("house1.png")
+#foreground = Image.open("house2.png")
+#background.paste(foreground, (0, 0), foreground)
+#background.show()	
