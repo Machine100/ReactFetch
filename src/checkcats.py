8f3353b8703cd7003fe20326b81b4c93adce670d
Machine100 <joesmileyyy@yahoo.com>
2019-04-15 14:49:39 -0400

    building checkcats

--- src/checkcats.py
+++ src/checkcats.py
@@ -3,15 +3,15 @@ import boto3
 s3client = boto3.client('s3')
 
 
-if state[things cat needs]:
+if state[cat needs]:
 	state[cat1: true]
-if state[things cat needs]:
+if state[cat needs]:
 	state[cat2: true]
-if state[things cat needs]:
+if state[cat needs]:
 	state[cat3: true]
-if state[things cat needs]:
+if state[cat needs]:
 	state[cat4: true]
-if state[things cat needs]:
+if state[cat needs]:
 	state[cat5: true]
 	
 
