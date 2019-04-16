2aa351f297e62e88c9287966ac3cc82139006c92
Machine100 <joesmileyyy@yahoo.com>
2019-04-16 15:43:57 -0400

    Update drawcats.py

--- src/drawcats.py
+++ src/drawcats.py
@@ -13,37 +13,37 @@ event = {"food1": True,
 
 mainimage = Image.open("house1.png")
 
-food1 = Image.open("food1.png")  
-food2 = Image.open("food2.png")
-toy1  = Image.open("toy1.png")
-toy2  = Image.open("toy2.png")
-toy3  = Image.open("toy3.png")
-cat1  = Image.open("cat1.png")
-cat2  = Image.open("cat2.png")
-cat3  = Image.open("cat3.png")
-cat4  = Image.open("cat4.png")
-cat5  = Image.open("cat5.png")
+imgfood1 = Image.open("food1.png")  
+imgfood2 = Image.open("food2.png")
+imgtoy1  = Image.open("toy1.png")
+imgtoy2  = Image.open("toy2.png")
+imgtoy3  = Image.open("toy3.png")
+imgcat1  = Image.open("cat1.png")
+imgcat2  = Image.open("cat2.png")
+imgcat3  = Image.open("cat3.png")
+imgcat4  = Image.open("cat4.png")
+imgcat5  = Image.open("cat5.png")
 
-if event[food1]:
-	mainimage.paste(food1, (0, 0), food1)
-if event[food2]:
-	mainimage.paste(food2, (0, 0), food2)
-if event[toy1]:
-	mainimage.paste(toy1,  (0, 0), toy1)
-if event[toy2]:
-	mainimage.paste(toy2,  (0, 0), toy2)
-if event[toy3]:
-	mainimage.paste(toy3,  (0, 0), toy3)
-if event[cat1]:
-	mainimage.paste(cat1,  (0, 0), cat1)
-if event[cat2]:
-	mainimage.paste(cat2,  (0, 0), cat2)
-if event[cat3]:
-	mainimage.paste(cat3,  (0, 0), cat3)
-if event[cat4]:
-	mainimage.paste(cat4,  (0, 0), cat4)
-if event[cat5]:
-	mainimage.paste(cat5,  (0, 0), cat5)
+if event['food1']:
+	mainimage.paste(imgfood1, (0, 0), imgfood1)
+if event['food2']:
+	mainimage.paste(imgfood2, (0, 0), imgfood2)
+if event['toy1']:
+	mainimage.paste(imgtoy1,  (0, 0), imgtoy1)
+if event['toy2']:
+	mainimage.paste(imgtoy2,  (0, 0), imgtoy2)
+if event['toy3']:
+	mainimage.paste(imgtoy3,  (0, 0), imgtoy3)
+if event['cat1']:
+	mainimage.paste(imgcat1,  (0, 0), imgcat1)
+if event['cat2']:
+	mainimage.paste(imgcat2,  (0, 0), imgcat2)
+if event['cat3']:
+	mainimage.paste(imgcat3,  (0, 0), imgcat3)
+if event['cat4']:
+	mainimage.paste(imgcat4,  (0, 0), imgcat4)
+if event['cat5']:
+	mainimage.paste(imgcat5,  (0, 0), imgcat5)
 
 mainimage.show()
 
