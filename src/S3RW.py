a102bea1c18e54448df658e69f4fc8d804a42c85
Machine100 <joesmileyyy@yahoo.com>
2019-05-16 10:20:44 -0400

    separating out the back end

--- src/S3RW.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import requests
-from io import BytesIO
-
-url = "https://s3.amazonaws.com/derffred/house1.png"
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
