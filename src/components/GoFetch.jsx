fe4a54fe161944c9db25e4324581ba5bf700103d
Machine100 <joesmileyyy@yahoo.com>
2019-05-24 12:08:02 -0400

    Update GoFetch.jsx

--- src/components/GoFetch.jsx
+++ src/components/GoFetch.jsx
@@ -4,13 +4,24 @@ class GoFetch extends Component {
 	state = {
 		monsterpic: 'https://s3.amazonaws.com/derffred/outputdestination.png'
 	}
+	
+	addFood1() {console.log('food1');}
+	addFood2() {console.log('food2');}
+	addToy1()  {console.log('toy1');}
+	addToy2()  {console.log('toy2');}
+	addToy3()  {console.log('toy3');}
+
+
 	render(){
-		const {count} = this.state
 		const {monsterpic} = this.state
 		return(
 			<React.Fragment>
-				//<img src={monsterpic} alt='' />
-				<button>addtoy</button>
+				<img src={monsterpic} alt='' />
+				<button onClick={this.addFood1}> addfood1 </button>
+				<button onClick={this.addFood2}> addfood2 </button>
+				<button onClick={this.addToy1}> addtoy1 </button>
+				<button onClick={this.addToy2}> addtoy2 </button>
+				<button onClick={this.addToy3}> addtoy3 </button>
 			</React.Fragment>
 		);
 	}
