54057b4c07647387e598bdb8456a8fd8a1164b2d
Machine100 <joesmileyyy@yahoo.com>
2019-05-29 15:07:00 -0400

    .

--- src/components/GoFetch.jsx
+++ src/components/GoFetch.jsx
@@ -2,26 +2,44 @@ import React, {Component} from 'react';
 
 class GoFetch extends Component {
 	state = {
-		monsterpic: 'https://s3.amazonaws.com/derffred/outputdestination.png'
+		worldview: 'https://s3.amazonaws.com/derffred/worldview.png'
 	}
 	
-	addFood1() {console.log('food1');}
-	addFood2() {console.log('food2');}
-	addToy1()  {console.log('toy1');}
-	addToy2()  {console.log('toy2');}
-	addToy3()  {console.log('toy3');}
+	addFood1() {
+		fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/addfood1')
+	}
 
+	addFood2() {
+		fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/addfood2')
+	}
+
+	addToy1()     {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/addtoy1')}
+	addToy2()     {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/addtoy2')}
+	addToy3()     {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/addtoy3')}
+	removeFood1() {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/removefood1')}
+	removeFood2() {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/removefood2')}
+	removeToy1()  {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/removetoy1')}
+	removeToy2()  {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/removetoy2')}
+	removeToy3()  {fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/removetoy3')}
+	generateWorld() {
+		fetch('https://tfbenvwogg.execute-api.us-east-1.amazonaws.com/stage1/generateworld')}
 
 	render(){
-		const {monsterpic} = this.state
+		const {worldview} = this.state
 		return(
 			<React.Fragment>
-				<img src={monsterpic} alt='' />
-				<button onClick={this.addFood1}> addfood1 </button>
-				<button onClick={this.addFood2}> addfood2 </button>
-				<button onClick={this.addToy1}> addtoy1 </button>
-				<button onClick={this.addToy2}> addtoy2 </button>
-				<button onClick={this.addToy3}> addtoy3 </button>
+				<img src={worldview} alt='' />
+				<button onClick={this.addFood1}> add food1 </button>
+				<button onClick={this.addFood2}> add food2 </button>
+				<button onClick={this.addToy1}> add toy1 </button>
+				<button onClick={this.addToy2}> add toy2 </button>
+				<button onClick={this.addToy3}> add toy3 </button>
+				<button onClick={this.removeFood1}> remove food1 </button>
+				<button onClick={this.removeFood2}> remove food2 </button>
+				<button onClick={this.removeToy1}> remove toy1 </button>
+				<button onClick={this.removeToy2}> remove toy2 </button>
+				<button onClick={this.removeToy3}> remove toy3 </button>
+				<button onClick={this.generateWorld}> generate world </button>
 			</React.Fragment>
 		);
 	}
